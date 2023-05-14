151888a9cc92edadc410c44e27947b52df27a28f
PikaPika <anjasroket@gmail.com>
2023-05-15 01:05:59 +0700

    celestia

--- public/generate.js
+++ public/generate.js
@@ -1,17 +1,12 @@
 function generate() {
-    const nBytes = new Uint8Array(8); // 8 random bytes
-    window.crypto.getRandomValues(nBytes);
-    const namespace_id = Array.from(nBytes, (byte) =>
-      byte.toString(16).padStart(2, "0")
-    ).join("");
-  
-    const lenMsg = Math.floor(Math.random() * 100);
-    const msgBytes = new Uint8Array(lenMsg);
-    window.crypto.getRandomValues(msgBytes);
-    const message = Array.from(msgBytes, (byte) =>
-      byte.toString(16).padStart(2, "0")
-    ).join("");
-  
-    document.getElementById("namespace_id").value = namespace_id;
-    document.getElementById("message").value = message;
-  }
+  const namespace_id = [...crypto.getRandomValues(new Uint8Array(8))]
+    .map(byte => byte.toString(16).padStart(2, '0'))
+    .join('');
+
+  const message = [...crypto.getRandomValues(new Uint8Array(Math.floor(Math.random() * 100)))]
+    .map(byte => byte.toString(16).padStart(2, '0'))
+    .join('');
+
+  document.getElementById('namespace_id').value = namespace_id;
+  document.getElementById('message').value = message;
+}
